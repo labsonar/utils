e3943d1fea1b0588fc6d190074709d9871fe007a
Fabio Oliveira <fabio.oliveira@lps.ufrj.br>
2024-09-25 14:42:00 +0000

    Adicionando funcao para encontrar arquivos
    Verificar extensao desejada, procura recursivamente

--- src/lps_utils/utils.py
+++ src/lps_utils/utils.py
@@ -4,6 +4,7 @@ Utility Module
 This module provides utility functions for general proposes.
 """
 import random
+import os
 import numpy as np
 
 def set_seed():
@@ -11,3 +12,14 @@ def set_seed():
     seed = 42
     random.seed(seed)
     np.random.seed(seed)
+
+
+def find_files(current_directory='.', extension = ".wav"):
+    wav_files = []
+
+    for root, _, files in os.walk(current_directory):
+        for file in files:
+            if file.endswith(extension):
+                wav_files.append(os.path.join(root, file))
+
+    return wav_files
